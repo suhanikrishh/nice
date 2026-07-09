30c1211598ad5b9e945a3f52fc6535a1959d6f6a
suhanikrishh <mechandoz360@gmail.com>
2026-07-09 18:15:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -87,3 +87,27 @@ const links = [
 document.querySelectorAll(".link-card").forEach((card, index) => {
     card.href = links[index];
 });
+
+/* ===========================
+      AUTO IMAGE SLIDER
+=========================== */
+
+const slides = document.querySelectorAll(".slide");
+
+let currentSlide = 0;
+
+setInterval(() => {
+
+    slides[currentSlide].classList.remove("active");
+
+    currentSlide++;
+
+    if(currentSlide >= slides.length){
+
+        currentSlide = 0;
+
+    }
+
+    slides[currentSlide].classList.add("active");
+
+},3000);
